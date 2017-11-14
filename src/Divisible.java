906b56fc8cf7dcdac582ca67208fb67f928fca54
mgh3326 <mgh3326@naver.com>
2017-11-15 08:15:28 +0900

    use array

--- src/Divisible.java
+++ src/Divisible.java
@@ -1,9 +1,20 @@
 import java.util.Arrays;
 
 class Divisible {
-    public int[] divisible(int[] array, int divisor) {
+    public int[] divisible(int[] array, int divisor) {//arry 사용
         //ret에 array에 포함된 정수중, divisor로 나누어 떨어지는 숫자를 순서대로 넣으세요.
-        int[] ret = new int[나누어 떨어지는 숫자의 개수];
+        int num=0;
+        int j=0;
+        for(int i=0;i<array.length;i++)
+        if(array[i]%divisor ==0)
+            num++;
+        int[] ret = new int[num];
+        for(int i=0;i<array.length;i++)
+        {
+            if(array[i]%divisor ==0)
+                ret[j++]=array[i];
+        }
+
         return ret;
     }
     // 아래는 테스트로 출력해 보기 위한 코드입니다.
